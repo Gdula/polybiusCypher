148393f824a6f6114a8b20fcde2a705dbc188ef0
Gdula <da.gdula@gmail.com>
2020-11-11 17:42:31 +0100

    First commit

---
+++ src/main/java/com/gdula/polybiusCypher/PolybiusCypherApplication.java
@@ -0,0 +1,13 @@
+package com.gdula.polybiusCypher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolybiusCypherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolybiusCypherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdula/polybiusCypher/PolybiusCypherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdula.polybiusCypher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolybiusCypherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
